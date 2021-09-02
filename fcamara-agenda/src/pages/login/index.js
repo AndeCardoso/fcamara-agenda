f813c99f657d06f62642af9918d80fa2eb64cb94
AndeCardoso <andersoncardoso.dev@gmail.com>
2021-09-02 19:52:21 -0300

    commit inicial com inicio de estruturacao do login

---
+++ fcamara-agenda/src/pages/login/index.js
@@ -0,0 +1,18 @@
+const Login = () => {
+    return (
+        <>
+            <wrapper>
+                <label>
+                    Nome:
+                    <input type="text" ></input>
+                </label>
+                <label>
+                    Senha:
+                    <input type="password" ></input>
+                </label>
+            </wrapper>
+        </>
+    );
+};
+
+export default Login;
