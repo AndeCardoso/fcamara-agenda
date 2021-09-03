b255694c6b736ee01f12c07421aa372fc6d56754
AndeCardoso <andersoncardoso.dev@gmail.com>
2021-09-03 13:22:14 -0300

    feat: adiciona tela de login com logica basica de autenticação

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,19 +1,41 @@
+import { useState } from 'react/cjs/react.development';
 import Button from '../dumb/button'
 import Input from '../dumb/input'
 import './style.css'
 
 const Login = () => {
+    const [email, setEmail] = useState();
+    const [senha, setSenha] = useState();
+
+    let usuario = [email, senha];
+    usuario.email = "admin";
+    usuario.senha = "admin";
+
+    const submitLogin = () => {
+        if (!email) {
+            alert("Informe um E-mail para fazer login!");
+        }
+        if (!senha) {
+            alert("Informe uma Senha para fazer login!");
+        } else {
+            if (email === usuario.email & senha === usuario.senha) {
+                alert( email + " logado com sucesso!");
+            }
+        }
+    };
+
     return (
         <div className="wrapper-login">
+            <h1>Login</h1>
             <label>
                 E-mail:
-                <Input type="text" onChange={""} />
+                <Input type="text" onChange={(event => setEmail(event.target.value))} />
             </label>
             <label>
                 Senha:
-                <Input type="password" onChange={""} />
+                <Input type="password" onChange={(event => setSenha(event.target.value))} />
             </label>
-            <Button type="submit" onClick="">Entrar</Button>
+            <Button type="submit" onClick={submitLogin}>Entrar</Button>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Login from './pages/login';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
   document.getElementById('root')
 );
